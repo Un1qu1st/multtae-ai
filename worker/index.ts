66d2ee3b89b3655d0329961eac3e55eaff8a0731
Un1qu1st <kanit7@naver.com>
2026-07-17 08:35:17 +0900

    Rename Worker source comment to 물살핌

--- worker/index.ts
+++ worker/index.ts
@@ -1,4 +1,4 @@
-/** Cloudflare Worker entry point for the public 물때AI pilot. */
+/** Cloudflare Worker entry point for the public 물살핌 pilot. */
 import handler from "vinext/server/app-router-entry";
 
 export default handler;
